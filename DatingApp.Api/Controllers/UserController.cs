90094e12ecd552f9cfc960473dc936be0863d592
Ayman-samir <samirbelal589@gmail.com>
2024-01-16 16:00:35 +0200

    first commit

---
+++ DatingApp.Api/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using DatingApp.Api.Data;
+using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var result=await _dbContext.Users.ToListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ DatingApp.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Api.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
